ce5a93798eb33bea78b150971b26612f48b4a18f
camillea <83574726+cmlya@users.noreply.github.com>
2021-08-13 10:27:18 -0400

    Starts game + colored log

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import time
 
 import cv2
-import numpy as np
-from python_imagesearch.imagesearch import imagesearch_v2, imagesearcharea
 import pyautogui
-from PIL import Image
+from python_imagesearch.imagesearch import imagesearcharea
 
+from colors import Colors
+
+from statistics import mean
+
+button_y = 1450
 check_x = 1120
-check_y = 1450
+check_y = button_y
 cross_x = 1480
-cross_y = 1450
+cross_y = button_y
+start_x = (cross_x + check_x) / 2
 first_line_top_x = 1000
 first_line_top_y = 500
 first_line_bottom_x = 1590
@@ -26,6 +30,8 @@ prompt_bounds = (
 
 templates = [cv2.imread(f"{i}.png", 0) for i in range(10)]
 operators = {op: cv2.imread(f"{op}.png", 0) for op in ["+", "-", "x", "div"]}
+time_elapsed = []
+
 
 # check_x = imagesearch("check.png")[0][0]
 # check_y = imagesearch("check.png")[0][1]
@@ -45,17 +51,23 @@ operators = {op: cv2.imread(f"{op}.png", 0) for op in ["+", "-", "x", "div"]}
 # nine_w = Image.open("9.png").width
 
 
+def start():
+    pyautogui.click(x=start_x, y=button_y)
+    print(Colors.EMERALD + Colors.UNDERLINE + "Game Started" + Colors.RESET)
+
+
 def click_check():
-    print("CORRECT")
+    print(Colors.GREEN + "CORRECT" + Colors.RESET)
     pyautogui.click(x=check_x, y=check_y)
 
 
 def click_cross():
-    print("INCORRECT")
+    print(Colors.RED + "INCORRECT" + Colors.RESET)
     pyautogui.click(x=cross_x, y=cross_y)
 
 
-pyautogui.sleep(5)
+pyautogui.sleep(4)
+start()
 while True:
     time1 = time.time()
     nums = {}
@@ -66,7 +78,7 @@ while True:
             *prompt_bounds
         )
         # coords = imagesearch(f"{i}.png")
-        print(len(coords))
+        # print(len(coords))
         for coord in coords:
             if coord[1] > first_line_bottom_y:
                 res[coord[0]] = i
@@ -90,29 +102,32 @@ while True:
         }[op]
         break
 
-    print("nums: " + str(nums))
-    print("res: " + str(res))
+    print(Colors.PINK + "nums: " + str(nums) + Colors.RESET)
+    print(Colors.PINK + "res: " + str(res) + Colors.RESET)
     expression = ""
     for i in sorted(nums.keys()):
-        expression += str(nums.get(i))
+        if str(nums.get(i)) == "+" or str(nums.get(i)) == "-" or str(nums.get(i)) == "*" or str(nums.get(i)) == "/":
+            expression += " " + str(nums.get(i)) + " "
+        else:
+            expression += str(nums.get(i))
         # print(i, end="")
     result = ""
     for i in sorted(res.keys()):
         result += str(res.get(i))
-    print("getting result", result)
+    # print("getting result", result)
     result = int(result)
 
-    print(expression)
-    print("= " + str(result))
+    print(Colors.BOLD + Colors.EMERALD_BG + expression + " = " + str(result) + Colors.RESET)
+    # print("= " + str(result))
 
     if "+" in expression:
         split = expression.split("+")
         x1 = int(split[0])
         x2 = int(split[1])
-        print("x1, x2:" + str(x1) + " + " + str(x2))
-        print("result: " + str(result))
+        # print("x1, x2: " + str(x1) + " + " + str(x2))
+        # print("result: " + str(result))
         is_correct = x1 + x2 == result
-        print("is correct: ", str(is_correct), f"({x1} + {x2} == {result})")
+        # print("is correct: ", str(is_correct), f"({x1} + {x2} = {result})")
         if x1 + x2 == result:
             click_check()
         else:
@@ -122,10 +137,10 @@ while True:
         split = expression.split("-")
         x1 = int(split[0])
         x2 = int(split[1])
-        print("x1, x2:" + str(x1) + " - " + str(x2))
-        print("result: " + str(result))
+        # print("x1, x2:" + str(x1) + " - " + str(x2))
+        # print("result: " + str(result))
         is_correct = x1 - x2 == result
-        print("is correct: ", str(is_correct), f"({x1} - {x2} == {result})")
+        # print("is correct: ", str(is_correct), f"({x1} - {x2} = {result})")
         if x1 - x2 == result:
             click_check()
         else:
@@ -135,10 +150,10 @@ while True:
         split = expression.split("*")
         x1 = int(split[0])
         x2 = int(split[1])
-        print("x1, x2:" + str(x1) + " * " + str(x2))
-        print("result: " + str(result))
+        # print("x1, x2:" + str(x1) + " * " + str(x2))
+        # print("result: " + str(result))
         is_correct = x1 * x2 == result
-        print("is correct: " + str(is_correct))
+        # print("is correct: ", str(is_correct), f"({x1} * {x2} = {result})")
         if x1 * x2 == result:
             click_check()
         else:
@@ -148,14 +163,18 @@ while True:
         split = expression.split("/")
         x1 = int(split[0])
         x2 = int(split[1])
-        print("x1, x2:" + str(x1) + " / " + str(x2))
-        print("result: " + str(result))
+        # print("x1, x2:" + str(x1) + " / " + str(x2))
+        # print("result: " + str(result))
         is_correct = x1 / x2 == result
-        print("is correct: " + str(is_correct))
+        # print("is correct: ", str(is_correct), f"({x1} / {x2} = {result})")
         if x1 / x2 == result:
             click_check()
         else:
             click_cross()
 
-    print("TIME ELAPSED:" + str(time.time() - time1) + " seconds")
-    print("-------------------------------")
+    duration = time.time() - time1
+    print(Colors.BLUE + "Time elapsed: " + str(duration) + " seconds" + Colors.RESET)
+    time_elapsed.append(duration)
+    print(Colors.BLUE + "Average move duration: " + str(mean(time_elapsed)) + " seconds" + Colors.RESET)
+    print(Colors.LEMON + Colors.LAVENDER_BG + "--------------------------------------------------" + Colors.RESET)
+    # time.sleep(1)
